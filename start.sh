c61972e7d8ac180cd4be3c8dfac0147cfc684d07
homeserversample <homeserversample@gmail.com>
2025-02-22 21:53:54 -0800

    cleaned up some command line arguments

--- start.sh
+++ start.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 # thank you to https://github.com/mjhwully for cap-add trick : https://github.com/MatthewVance/unbound-docker-rpi/issues/4
-sudo docker run                                    \
-    -d                                             \
-    -it                                            \
-    --rm                                           \
-    --network host                                 \
-    --cap-add NET_ADMIN                            \
-    -v ./conf:/conf                                \
-    docker.io/homeserversample/unbound             \
-        unbound -d -c /conf/pi-hole.conf
+sudo docker run --detach                                 \
+                --rm                                     \
+                --network host                           \
+                --cap-add NET_ADMIN                      \
+                --volume ./conf:/conf                    \
+                docker.io/homeserversample/unbound       \
+                    unbound -d -c /conf/pi-hole.conf
