4992ae809b0d140847b12f01d48ce52555451317
homeserversample <homeserversample@gmail.com>
2025-02-22 19:25:20 -0800

    makde docker cmd multiline

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
 # thank you to https://github.com/mjhwully for cap-add trick : https://github.com/MatthewVance/unbound-docker-rpi/issues/4
-sudo docker run -it -p 5335:5335/tcp -p 5335:5335/udp --cap-add NET_ADMIN -v ./conf:/conf unbound unbound -d -vvvvvv -c /conf/pi-hole.conf
+sudo docker run                                    \
+    -d                                             \
+    -it                                            \
+    --rm                                           \
+    -p 5335:5335/tcp                               \
+    -p 5335:5335/udp                               \
+    --cap-add NET_ADMIN                            \
+    -v ./conf:/conf                                \
+    docker.io/homeserversample/unbound             \
+        unbound -d -c /conf/pi-hole.conf
