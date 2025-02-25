5bdf8c1c39d559c2514413eb234d2bf3466e3948
homeserversample <homeserversample@gmail.com>
2025-02-25 15:17:28 -0800

    changing image name and adding build.sh script

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 # thank you to https://github.com/mjhwully for cap-add trick : https://github.com/MatthewVance/unbound-docker-rpi/issues/4
-# to use a locally built container, change "docker.io/homeserversample/unbound" to "unbound"
 sudo docker run --detach                                 \
                 --restart unless-stopped                 \
                 --network host                           \
                 --cap-add NET_ADMIN                      \
                 --volume ./conf:/conf                    \
-                docker.io/homeserversample/unbound       \
+                unb                                      \
                     unbound -d -c /conf/localhost.conf
