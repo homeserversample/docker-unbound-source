f72c3f307b94f6a333a7897e122f17bfa7c07e9e
homeserversample <homeserversample@gmail.com>
2025-02-26 17:20:11 -0800

    got logging working; switching to docker compose and away from scripts

--- start.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# thank you to https://github.com/mjhwully for cap-add trick : https://github.com/MatthewVance/unbound-docker-rpi/issues/4
-sudo docker run --detach                                 \
-                --restart unless-stopped                 \
-                --network host                           \
-                --cap-add NET_ADMIN                      \
-                --volume ./conf:/conf                    \
-                unb                                      \
-                    unbound -d -c /conf/localhost.conf
